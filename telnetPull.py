c8150798122b0a265a56406b42deb6218ac10504
John Paul Almonte <almonte.breta-john656@mail.kyutech.jp>
2023-04-25 14:12:34 +0900

    handled appended frame escape/end by KISS

--- telnetPull.py
+++ telnetPull.py
@@ -1,24 +1,42 @@
 import socket
-import asyncio
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 8100  # Port to listen on (non-privileged ports are > 1023)
-'''
+
+# LISTEN TO KISS PORT OF AX.25 HS SOUNDMODEM
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print('Connected!')
     data = s.recv(108).hex()
 
-print(data)
-'''
-data = 'c0004a4736594257304c4f52414d4f303ef0fff0ff0000012d225b1d5d2800124e3087f4b3318f78302c90017d3680c190ea10dbdd4177feabffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc0'
-
-print(len(data))
-print(data[0:48])
 appendedFrame = len(data) - 212
-print(appendedFrame)
-DATA_PACKET = data[48:112+appendedFrame]
-print(DATA_PACKET)
-# SEARCH FOR DBDD AND C000 
-# GET ITS INDEX
-# REPLACE DBDD WITH DB AND C000 WITH C0
+print('Number of Appended Frames: {} bytes' .format(int(appendedFrame/2)))
+DATA_PACKET_KISS = data[48:112+appendedFrame].upper()
+print('Original Data with Appended Frames \n',DATA_PACKET_KISS)
+print('Data Length: {} bytes'.format(int(len(DATA_PACKET_KISS)/2)))
+
+# REMOVE APPENDED TRANSPOSED FRAME ESCAPE BY KISS PROTOCOL I.E. CHANGE DBDD TO DB
+DATA_PACKET_DBDD = ''
+DATA_PACKET_KISS = DATA_PACKET_KISS.split('DBDD')
+for i in range(len(DATA_PACKET_KISS)):
+    if(i == len(DATA_PACKET_KISS) - 1):
+        DATA_PACKET_DBDD = DATA_PACKET_DBDD + DATA_PACKET_KISS[i]
+        break
+    DATA_PACKET_DBDD = DATA_PACKET_DBDD + DATA_PACKET_KISS[i] + 'DB'
+
+# REMOVE APPENDED TRANSPOSED FRAME END BY KISS PROTOCOL I.E. CHANGE C000 TO C0
+DATA_PACKET_C000 = ''
+DATA_PACKET_DBDD = DATA_PACKET_DBDD.split('C000')
+for j in range(len(DATA_PACKET_DBDD)):
+    if(j == len(DATA_PACKET_DBDD) - 1):
+        DATA_PACKET_C000 = DATA_PACKET_C000 + DATA_PACKET_DBDD[j]
+        break
+    DATA_PACKET_C000 = DATA_PACKET_C000 + DATA_PACKET_DBDD[j] + 'C0'
+
+DATA_PACKET = DATA_PACKET_C000
+
+print('Corrected Data \n', DATA_PACKET)
+print('Data Length: {} bytes'.format(int(len(DATA_PACKET)/2)))
+
+DATA_PACKET = (int(DATA_PACKET,16)) # CONVERT TO INT FOR PACKET DECODER
+print(hex(DATA_PACKET))
